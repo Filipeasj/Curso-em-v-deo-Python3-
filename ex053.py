77daf468bc6267fc0f27f6f5d20260c497fea64b
Filipe Alves <202421190039@ifba.edu.br>
2024-11-04 09:53:34 -0300

    Delete ex053.py

--- ex053.py
+++
@@ -1,7 +0,0 @@
-frase=str(input('Digite uma frase:')).strip()
-frase=frase.replace(' ','')
-frase_invertida = frase[::-1]
-if frase_invertida==frase:
-    print('é um palíndromo')
-else:
-    print('não é um palíndromo')
