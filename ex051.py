5ab1ba96d0b82eedd3656f91757cf4612667d508
Filipe Alves <202421190039@ifba.edu.br>
2024-11-04 09:52:02 -0300

    Delete ex051.py

--- ex051.py
+++
@@ -1,5 +0,0 @@
-FirstTerm=int(input('Escolha o primneiro termo: '))
-reason=int(input('Escolha a razão da P.A: '))
-for c in range(0,10):
-    value=FirstTerm+reason*c
-    print('a{} = {}' .format(c+1,value))
