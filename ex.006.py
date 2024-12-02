d2141a4d1efd799c47ca98725f7585bd7b9215ed
Filipe Alves <202421190039@ifba.edu.br>
2024-12-02 11:58:22 -0300

    Update and rename ex.006.py to ex006.py

--- ex.006.py
+++
@@ -1,2 +0,0 @@
-n=int(input('Insira um número: '))
-print('dobro:{} \ntriplo:{} \nraiz quadrada:{:.0f}'.format(n*2,n*3,n**(1/2)))
---
+++ ex006.py
@@ -0,0 +1,2 @@
+n=int(input('Insira um número: '))
+print('dobro:{} \ntriplo:{} \nraiz quadrada:{:.0f}'.format(n*2,n*3,n**(1/2)))
